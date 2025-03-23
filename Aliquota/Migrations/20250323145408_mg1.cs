8a4b95dd26f622b20c5f478a6095e8fd8495eac4
JoaoVitor-dev <joaovitorreal@outlook.com>
2025-03-23 11:56:22 -0300

    fix/downgrade pacotes nuget EF Core

---
+++ Aliquota/Migrations/20250323145408_mg1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Aliquota.Migrations
+{
+    /// <inheritdoc />
+    public partial class mg1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Fundos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fundos", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Aplicacoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Valor = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    DataAplicacao = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    FundoId = table.Column<int>(type: "int", nullable: false),
+                    ClienteId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aplicacoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Aplicacoes_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Aplicacoes_Fundos_FundoId",
+                        column: x => x.FundoId,
+                        principalTable: "Fundos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Resgates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Valor = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    ImpostoDeRenda = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    DataResgate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    FundoId = table.Column<int>(type: "int", nullable: false),
+                    ClienteId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Resgates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Resgates_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Resgates_Fundos_FundoId",
+                        column: x => x.FundoId,
+                        principalTable: "Fundos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Clientes",
+                columns: new[] { "Id", "Nome" },
+                values: new object[,]
+                {
+                    { 1, "João" },
+                    { 2, "Maria" },
+                    { 3, "José" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Fundos",
+                columns: new[] { "Id", "Nome" },
+                values: new object[,]
+                {
+                    { 1, "Fundo de Investimento A" },
+                    { 2, "Fundo de Investimento B" },
+                    { 3, "Fundo de Investimento C" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Aplicacoes_ClienteId",
+                table: "Aplicacoes",
+                column: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Aplicacoes_FundoId",
+                table: "Aplicacoes",
+                column: "FundoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resgates_ClienteId",
+                table: "Resgates",
+                column: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resgates_FundoId",
+                table: "Resgates",
+                column: "FundoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aplicacoes");
+
+            migrationBuilder.DropTable(
+                name: "Resgates");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Fundos");
+        }
+    }
+}
